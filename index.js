ef24d3d33de1ca090fe4c1009a78675e2f45702e
Orry Baram <orrybaram@gmail.com>
2016-06-24 12:42:57 -0400

    remove webpack

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ exports.observer = observer;
 function observer (target) {
   const _render = target.render;
   const _beforeUnmount = target.beforeUnmount;
-  let _unsubscriber = null;
-  let _updateCount = 0;
+  let unsubscriber = null;
+  let updateCount = 0;
 
   target.render = (component, setState) => {
     let result;
-    if (_unsubscriber) {
-      _unsubscriber();
+    if (unsubscriber) {
+      unsubscriber();
     }
-    _unsubscriber = autorun(() => {
+    unsubscriber = autorun(() => {
       result = _render(component, setState);
-      setState({ __updates: _updateCount++ });
+      setState({ __updates: updateCount++ });
     });
     return result;
   };
 
   target.beforeUnmount = (component, el) => {
-    if (_unsubscriber) {
-      _unsubscriber();
+    if (unsubscriber) {
+      unsubscriber();
     }
     if (_beforeUnmount) {
       _beforeUnmount(component, el);
@@ -30,18 +30,18 @@ function observer (target) {
   };
 
   // If there's already a shouldUpdate, don't overwrite it.
-  target.shouldUpdate = target.shouldUpdate || _shouldUpdate;
+  target.shouldUpdate = target.shouldUpdate || shouldUpdate;
 
   return target;
 }
 
-function _shouldUpdate (component, nextProps, nextState) {
+function shouldUpdate (component, nextProps, nextState) {
   const {props, state} = component;
-  // update on any state changes (as is the default)
+  // Update on any state changes (as is the default)
   if (state !== nextState) {
     return true;
   }
-  // update if props are shallowly not equal, inspired by PureRenderMixin
+  // Update if props are shallowly not equal, inspired by PureRenderMixin
   let keys = Object.keys(props);
 
   if (keys.length !== Object.keys(nextProps).length) {
